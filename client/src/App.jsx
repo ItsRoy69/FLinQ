7c2d974bd6780e53c0b7b3527b8b1a995fb9f5f9
Srinkk <shreyaganguly2003@gmail.com>
2024-01-03 23:52:33 +0530

    Update events page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/home/Home";
 import NavBar from "./constants/navbar/NavBar";
+import Event from "./pages/events/Event";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/events" element= {<Event/>}/>
       </Routes>
     </Router>
   );
