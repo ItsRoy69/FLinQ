d2108d46a6fee029861109f9ba9af5817c124a3e
Shreya Ganguly <93050853+Srinkk@users.noreply.github.com>
2024-02-02 21:19:23 +0530

    Update userContext.jsx

--- client/src/contexts/userContext.jsx
+++ client/src/contexts/userContext.jsx
@@ -4,28 +4,43 @@ const UserContext = React.createContext()
 
 export function UserProvider({ children }) {
 
-    const [openauthmodal, setopenauthmodal] = useState(false);
-    const [openprofmodal, setopenprofmodal] = useState(false);
     const [isuserreg, setisuserreg] = useState(true);
     const [isuser, setisuser] = useState(false);
-    const [isprofreg, setisprofreg] = useState(true);
-
-
-
+ 
     const value = {
-        openauthmodal,
-        setopenauthmodal,
         isuserreg,
         setisuserreg,
         isuser,
         setisuser,
-        isprofreg,
-        setisprofreg
+    }
+    const emptyUser = {
+        email: "",
+        password: "",
+        username: "",
+        address: "",
+        phone: "",
+        name: "",
+        profession: "",
+    }
+    const [user, setUser] = useState(emptyUser)
 
+    const updateUser = (user) => {
+        setUser({
+            email : user.email,
+            password: user.password,
+            username : user.username,
+            address:user.address,
+            phone:user.phone,
+            name:user.name,
+            profession:user.profession
+        })
 
     }
+    const logout = () => {
+        setUser(emptyUser)
+    }
     return (
-        <UserContext.Provider value={value}>
+        <UserContext.Provider value={{user, updateUser, logout}}>
             {children}
         </UserContext.Provider>
     )
