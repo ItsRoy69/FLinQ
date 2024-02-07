88c326f2d43ad1f42d07f7d10b0b1bdb38ef8327
itsroy69 <jyotirmoyroy649@gmail.com>
2024-02-07 09:29:37 +0530

    added socket
    Signed-off-by: itsroy69 <jyotirmoyroy649@gmail.com>

--- server/index.js
+++ server/index.js
@@ -2,7 +2,14 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 const mongoose = require('mongoose');
+const http = require('http');
+const socketIo = require('socket.io');
+
+
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
 const PORT = process.env.PORT || 5000;
 const userRoute = require('./routes/userRoute');
 const postRoute = require('./routes/postRoute');
@@ -19,6 +26,19 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => res.send("Welcome to Flinq server"));
 app.use('/user', userRoute);
 app.use('/post', postRoute);
+
+// Socket.IO setup
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg); 
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
 
 const connectToDb = async () => {
     try {
