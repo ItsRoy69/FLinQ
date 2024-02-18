ce91c92347e50a846284c352e717a93da4ffef9f
itsroy69 <jyotirmoyroy649@gmail.com>
2024-02-18 12:13:12 +0530

    group creating and sending messages in group done
    Signed-off-by: itsroy69 <jyotirmoyroy649@gmail.com>

--- server/index.js
+++ server/index.js
@@ -13,15 +13,14 @@ const PORT = process.env.PORT || 5000;
 const userRoute = require('./routes/userRoute');
 const postRoute = require('./routes/postRoute');
 const chatRoute = require('./routes/chatRoute');
+const groupRoute = require('./routes/groupRoute');
 
-
+// Socket Chat
 const chatNamespace = io.of('/chat');
-
 chatNamespace.on('connection', (socket) => {
   console.log('A user connected to the chat namespace');
 
   socket.on('chat message', (msg) => {
-    // Your socket handling logic here
     chatNamespace.emit('chat message', msg);
   });
 
@@ -30,10 +29,34 @@ chatNamespace.on('connection', (socket) => {
   });
 });
 
+const chatGroupspace = io.of('/group');
+chatGroupspace.on('connection', (socket) => {
+  console.log('A user connected to the chat Groupspace');
+
+  socket.on('joinGroup', (groupId) => {
+    socket.join(groupId);
+    console.log(`User joined group ${groupId}`);
+  });
+
+  socket.on('chat message', (data) => {
+    chatGroupspace.to(data.groupId).emit('chat message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected from the chat Groupspace');
+  });
+});
+
+// Middleware to set io globally
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
 // Middlewares
 app.use(cors({
-    credentials: true,
-    origin: ["http://localhost:5173"]
+  credentials: true,
+  origin: ["http://localhost:5173"]
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -43,20 +66,21 @@ app.get('/', (req, res) => res.send("Welcome to Flinq server"));
 app.use('/user', userRoute);
 app.use('/post', postRoute);
 app.use('/chat', chatRoute);
+app.use('/group', groupRoute);
 
 const connectToDb = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_DB_URI);
-        console.log("MongoDB connected successfully");
-    } catch (error) {
-        console.error("MongoDB connection error:", error);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_DB_URI);
+    console.log("MongoDB connected successfully");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
 }
 
 connectToDb();
 
 // Listen to PORT
 server.listen(PORT, () => {
-    console.log(`Server is running @ PORT ${PORT}`);
+  console.log(`Server is running @ PORT ${PORT}`);
 });
