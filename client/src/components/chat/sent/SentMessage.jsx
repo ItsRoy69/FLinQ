19ae3aaa5c55590aa20b6df9de73f96eb299180c
Srinkk <shreyaganguly2003@gmail.com>
2024-02-22 13:55:11 +0530

    minor updates

--- client/src/components/chat/sent/SentMessage.jsx
+++ client/src/components/chat/sent/SentMessage.jsx
@@ -1,9 +1,12 @@
 import "./sentMessage.css";
 import {useState, useEffect} from 'react';
+import { useContext } from "react";
 import axios from 'axios'
+import { UserContext } from "../../../contexts/userContext";
 
 const SentMessage = ({ message }) => {
   const [username, setUsername] = useState('');
+  const { user } = useContext(UserContext)
 
   // if(message.sender != "Bot")  {
   //     const userId = message.sender
@@ -23,7 +26,7 @@ const SentMessage = ({ message }) => {
       <div className="flex justify-end">
         <div className="sent-message-container h-fit w-fit max-w-72 p-4  bg-gradient-to-t from-violet-900 to-pink-700 flex flex-col gap-3 justify-center rounded-t-2xl rounded-bl-2xl">
           <div className="sent-message-header font-semibold text-pink-400">
-            {message.sender || username}
+            { user.username}
           </div>
           <div className="sent-message-body">{message.message||message.text}</div>
         </div>
