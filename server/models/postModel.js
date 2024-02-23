f18c672e8a587b5bd137d5e639fb55a2d3f15fbb
Rajarshi Dutta <gogolmf74@gmail.com>
2024-02-23 13:55:28 +0530

    CommentModal in process
    Signed-off-by: Rajarshi Dutta <gogolmf74@gmail.com>

--- server/models/postModel.js
+++ server/models/postModel.js
@@ -12,7 +12,7 @@ const commentSchema = new mongoose.Schema({
     },
     content: {
         type: String,
-        required: false
+        required: true
     },
     createdAt: {
         type: Date,
